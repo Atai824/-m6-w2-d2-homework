e532660fa5577857f6541d1afa89a4f205d9af3e
Atai824 <a.toktosunov.8207@westcliff.edu>
2025-10-16 05:13:03 -0700

    Week6 Day2 Homework done

--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -2,22 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Counter extends React.Component {
+
     increment = () => {
-    // fill in later
+        this.props.dispatch({ type: 'INCREMENT' });
   };
 
   decrement = () => {
-    // fill in later
+    this.props.dispatch({ type: 'DECREMENT' });
+  };
+
+  reset = ()=> {
+    this.props.dispatch({
+        type: 'RESET' 
+    });
   };
 
     render() {
         return (
-            <div>
-                <h2>Counter</h2>
-                <div>
-                    <span>{this.props.count}</span>
-                    <button onClick={this.decrement}>−</button>
-                    <button onClick={this.increment}>+</button>
+            <div className="card m-3" style={{ maxWidth: 360 }}>
+                <div className="card-header fw-bold">Counter</div>
+                <div className="card-body bg-info p-3">
+                    <span className="badge bg-light text-dark me-2">{this.props.count}</span>
+                    <button className="btn btn-sm btn-light me-2" onClick={this.decrement}>−</button>
+                    <button className="btn btn-sm btn-light me-2" onClick={this.increment}>+</button>
+                    <button className="btn btn-sm btn-outline-secondary" onClick={this.reset}>reset</button>
                 </div>
             </div>
         );
